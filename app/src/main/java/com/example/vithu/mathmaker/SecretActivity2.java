dff49d41f93a18448fdfc82cc9f94377a7adc9fa
Vithuran Sadagopan <Vithuran Sadagopan>
2016-09-28 00:27:56 -0400

    Percentage Calculator Works and open secret page

---
+++ app/src/main/java/com/example/vithu/mathmaker/SecretActivity2.java
@@ -0,0 +1,13 @@
+package com.example.vithu.mathmaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecretActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret2);
+    }
+}
